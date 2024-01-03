446cea3d4f3458d0a7a495287c3d61e8a82038a5
nobody <zky1024@users.noreply.github.com>
2024-01-03 10:02:31 +0800

    auto generate folders and files

--- src/main/java/util/LCUtil.java
+++ src/main/java/util/LCUtil.java
@@ -1,18 +1,26 @@
 package util;
 
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+
 public class LCUtil {
 
-    public static void generateNewNames(String s) {
-        // transform a string to a suitable java directory name
-        // 2558. Take Gifts From the Richest Pile => lc2558_take_gifts_from_the_richest_pile
-        System.out.println("lc" + s.toLowerCase().replace(" ", "_").replace(".", ""));
-        System.out.println();
-        // extract the number from a string
-        // 2558. Take Gifts From the Richest Pile => 2558
-        System.out.println("SolutionLC" + s.replaceAll("[^0-9]", ""));
+    public static String generateFolderName(String s) {
+        return "lc" + s.toLowerCase().replace(" ", "_").replace(".", "");
+    }
 
-        System.out.println();
-        System.out.println("TestLC" + s.replaceAll("[^0-9]", ""));
+
+    public static String generateSolutionName(String s) {
+        return "SolutionLC" + s.replaceAll("[^0-9]", "");
+    }
+
+
+    public static String generateTestName(String s) {
+        return "TestLC" + s.replaceAll("[^0-9]", "");
     }
 
     public static void main(String[] args) {
